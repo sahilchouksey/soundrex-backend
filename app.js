a6bca7287df4698fa2518bde87a7617d3823f4e3
Sahil Chouksey <59500196+sahilchouksey@users.noreply.github.com>
2022-12-04 09:16:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
+      "http://localhost:3001",
       "https://soundrex.netlify.app",
       "https://master--soundrex.netlify.app",
     ],
